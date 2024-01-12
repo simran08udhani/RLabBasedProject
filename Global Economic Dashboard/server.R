c5fa28b0ca9f53310d3b3231842ab2d5883e7cad
Simran Udhani <95423540+simran08udhani@users.noreply.github.com>
2024-01-12 15:30:11 +0530

    Add files via upload

---
+++ Global Economic Dashboard/server.R
@@ -0,0 +1,145 @@
+library("DT")
+library("tidyverse")
+library(shiny)
+library("scatterplot3d")
+library("rgl")
+
+library(randomForest)
+# Load your data and model from "project.R"
+source("ML_education_expenditure_model.R")
+
+countries <- read_csv("data/cn_filter.csv")
+data <- df # Assuming df is loaded from project.R
+rf_model <- rf_model # Assuming your_rf_model is loaded from project.R
+
+function(input, output, session) {
+  observeEvent(input$update_3d_visualization, {
+  output$visualization_3d <- renderPlot({
+    selected_indicator <- input$select_indicator_3d
+    selected_year <- input$select_year_3d
+    
+    if (selected_indicator == "scatterplot3d") {
+      if (selected_year == 2012) {
+        scatterplot3d(
+          x = countries$`Agriculture (% GDP)`[countries$Year == 2012],
+          y = countries$`Industry (% GDP)`[countries$Year == 2012],
+          z = countries$`Service (% GDP)`[countries$Year == 2012],
+          color = rainbow(sum(countries$Year == 2012)),
+          main = "3D Scatterplot - 2012",
+          xlab = "Agriculture (%GDP)",
+          ylab = "Industry (%GDP)",
+          zlab = "Service (%GDP)",
+          pch = 17
+        )
+      } else if (selected_year == 2022) {
+        scatterplot3d(
+          x = countries$`Agriculture (% GDP)`[countries$Year == 2022],
+          y = countries$`Industry (% GDP)`[countries$Year == 2022],
+          z = countries$`Service (% GDP)`[countries$Year == 2022],
+          color = rainbow(sum(countries$Year == 2022)),
+          main = "3D Scatterplot - 2022",
+          xlab = "Agriculture (%GDP)",
+          ylab = "Industry (%GDP)",
+          zlab = "Service (%GDP)",
+          pch = 17
+        )
+      }
+    }
+  })
+  
+  output$rgl_plot <- renderRglwidget({
+    selected_indicator <- input$select_indicator_3d
+    selected_year <- input$select_year_3d
+    
+    if (selected_indicator == "rgl") {
+      if (selected_year == 2012) {
+        with(countries[countries$Year == 2012, ], {
+          rgl.open()
+          plot3d(
+            x = `Agriculture (% GDP)`,
+            y = `Industry (% GDP)`,
+            z = `Service (% GDP)`,
+            col = "blue",
+            type = "s",
+            size = 1.5,
+            xlab = "Agriculture as % of GDP",
+            ylab = "Education Expenditure as % of GDP",
+            zlab = "Health Expenditure as % of GDP"
+          )
+          rglwidget()
+        })
+      } else if (selected_year == 2022) {
+        with(countries[countries$Year == 2022, ], {
+          rgl.open()
+          plot3d(
+            x = `Agriculture (% GDP)`,
+            y = `Industry (% GDP)`,
+            z = `Service (% GDP)`,
+            col = "blue",
+            type = "s",
+            size = 1.5,
+            xlab = "Agriculture as % of GDP",
+            ylab = "Education Expenditure as % of GDP",
+            zlab = "Health Expenditure as % of GDP"
+          )
+          rglwidget()
+        })
+      }
+    }
+  })
+})
+  
+  output$data_tab <- renderDT({
+    as_tibble(countries) %>%
+      datatable(rownames = input$show_rownames, extensions="Responsive")
+  })
+ 
+      updateSelectInput(session,
+                        "select_country_with_updateSelectInput",
+                        choices = unique(countries$`Country Name`))
+      observeEvent(input$update_visualization, {
+        
+      
+      output$visualization_tab <- renderPlot({
+        selected_country <- input$select_country_with_updateSelectInput
+        selected_indicator <- input$selected_indicator_labelled_manual
+        
+        selected_data <- countries %>%
+          filter(`Country Name` == selected_country) %>%
+          select(Year, !!sym(selected_indicator)) %>%
+          drop_na()
+        
+        ggplot(selected_data, aes(x = Year, y = !!sym(selected_indicator))) +
+          geom_line() +
+          labs(
+            title = paste(selected_indicator, "in", selected_country),
+            x = "Year",
+            y = selected_indicator
+          )
+      })
+    
+  })
+  
+  
+  observeEvent(input$predict_button, {
+    isolate({
+      if (!is.null(input$Export....GDP.) && !is.null(input$Health.Expenditure....GDP.) && !is.null(input$Import....GDP.) && !is.null(input$Industry....GDP.) && !is.null(input$Inflation.Rate)) {
+        # Create a new data frame with the numeric Age, Category, Size, Gender, and Channel
+        new_data <- data.frame( Export....GDP.= input$Export....GDP., Health.Expenditure....GDP. = input$Health.Expenditure....GDP., Import....GDP. = input$Import....GDP., Industry....GDP. = input$Industry....GDP., Inflation.Rate = input$Inflation.Rate)
+        
+        # Predict using the random forest model
+        predictions <- predict(rf_model, new_data)
+        # Extract the first prediction
+        prediction <- predictions[1]
+        output$prediction_result <- renderText({
+          paste("Predicted Amount:", round(as.numeric(prediction), 2))
+        })
+      } else {
+        output$prediction_result <- renderText({
+          "Please enter Age, Category, Size, Gender, and Channel to predict the Amount."
+        })
+      }
+    })
+  })
+  
+}
---
+++ Global Economic Dashboard/ui.R
@@ -0,0 +1,118 @@
+library(shiny)
+library(shinydashboard)
+library("leaflet")
+library("DT")
+library("scatterplot3d")
+library("rgl")
+library("tidyverse")
+library(shinycustomloader)
+
+source("ML_education_expenditure_model.R")
+countries <- read_csv("data/cn_filter.csv")
+data <- df
+rf_model <- rf_model
+
+fluidPage(
+  theme = "custom.css",
+  titlePanel("Global Economic Dashboard", windowTitle = "Global Economic Dashboard"),
+  sidebarLayout(
+    sidebarPanel(
+      wellPanel(
+        h4("Welcome to the Global Economic Dashboard!"),
+        p("This interactive Shiny app provides a comprehensive overview of key economic indicators for a selection of countries."),
+        p("Explore data on agriculture, ease of doing business, education expenditure, exports, GDP, health expenditure, imports, industry, inflation rate, R&D, service sector, unemployment, population, land area, and more."),
+        p("Navigate through the tabs to visualize and analyze the economic performance of countries such as Argentina, Australia, Brazil, Canada, China, France, Germany, India, Indonesia, Italy, Japan, Korea, Mexico, Saudi Arabia, South Africa, Turkey, United Arab Emirates, United Kingdom, and the United States."),
+        p("Use the interactive features to compare countries, track trends over the years, and gain insights into the economic landscapes across continents."),
+        p("Whether you are a policymaker, researcher, or curious enthusiast, this Global Economic Dashboard offers a user-friendly interface to explore and understand the dynamics of these diverse economies."),
+        p("Start your exploration now and uncover the stories behind the numbers!")
+      )
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Data",
+                 checkboxInput("show_rownames", label = "Show rownames?"),
+                 DTOutput("data_tab")
+        ),
+        tabPanel("Visualization",
+                 fluidPage(
+                   fluidRow(
+                     column(
+                       selectInput("select_country_with_updateSelectInput",
+                                   label = "Choose a country:",
+                                   choices = NULL), width = 12
+                     ),
+                     column(
+                       selectInput(
+                         "selected_indicator_labelled_manual",
+                         "Select an indicator to display",
+                         choices = c(
+                           "Health Expenditure (% GDP)",
+                           "Education Expenditure (% GDP)",
+                           "Export (% GDP)",
+                           "Ease of Doing Business",
+                           "GDP",
+                           "R&D"
+                         ),
+                         width = "100%"
+                       ),
+                       width = 6
+                     
+                   ),
+                   # Add an action button for updating the chart
+                   column(
+                     offset = 4,
+                     actionButton("update_visualization", "Update Chart"),
+                     br(),
+                     width = 10
+                   )
+                   ),
+                   br(),
+                   plotOutput("visualization_tab")
+                 )
+        ),
+        tabPanel("3D Visualization",
+                 fluidPage(
+                   fluidRow(
+                     column(
+                       selectInput("select_indicator_3d",
+                                   label = "Select Indicator",
+                                   choices = c("scatterplot3d", "rgl")),
+                       width = 6
+                     ),
+                     column(
+                       selectInput("select_year_3d",
+                                   label = "Select Year",
+                                   choices = c(2012, 2022),
+                                   selected = 2012),
+                       width = 6
+                     ),
+                   # Add an action button for updating the 3D visualization
+                   column(
+                     offset = 4,
+                     actionButton("update_3d_visualization", "Update 3D Chart"),
+                     br(),
+                     width = 11
+                   )
+                   
+                   ),
+                   
+                   br(),
+                   withLoader(rglwidgetOutput("rgl_plot")),
+                   withLoader(plotOutput("visualization_3d"))
+                 )
+        ),
+        tabPanel("Future Trends",
+                 numericInput("Export....GDP.", "Export (% GDP)", min = 10.20, max = 98.34, value = 30.54),
+                 numericInput("Health.Expenditure....GDP.", "Health Expenditure (% GDP):", min = 2.668, max = 18.816, value = 7.864),
+                 numericInput("Import....GDP.", "Import (% GDP)", min = 11.78, max = 74.46, value = 28.75),
+                 numericInput("Industry....GDP.", "Industry (% GDP)", min = 16.50, max = 62.21, value = 28.75),
+                 numericInput("Inflation.Rate", "Inflation Rate", min = -2.093, max = 72.309, value = 4.081),
+                 actionButton("predict_button", "Predict"),
+                 br(),
+                 h3("Random Forest Model Results:"),
+                 textOutput("prediction_result")
+        )
+      )
+    )
+  )
+)
